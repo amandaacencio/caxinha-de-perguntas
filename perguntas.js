6895fd610aad154f78e43f1e6adc014ce4d9f9ed
amandaacencio <amanda.acencio@escola.pr.gov.br>
2025-09-23 09:10:47 -0300

    gjgjgjgfj

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+criaCartao(
+    'country life'  
+  'como funciona a agricultura?'
+  'A plantação de milho e soja envolve o preparo do solo, a escolha da semente adequada para as condições locais, o plantio com máquinas específicas (plantadeiras) e o cuidado constante(tratos culturais) para proteger as plantas de pragas e doenças.Por fim, a colheita é realizada quando os grãos atingem o ponto ideal, garantindo a produtividade da lavoura.'
+
+)
+criaCartao(
+    'country life'
+   'Como um pulverizador funciona?'
+   'O funcionamento de um pulverizador agrícola é baseado em um sistema de pressurização que força o líquido (solução de produtos químicos diluídos em água) a passar por um bico de pulverização, transformando-o em pequenas gotas distribuídas uniformemente sobre as plantas.'
+)
+criaCartao(
+'country life'
+'Como é o processo de preparação da terra para o plantio? Quais técnicas são utilizadas?'
+'>O preparo da terra para o plantio envolve técnicas para melhorar a estrutura, a fertilidade e
+                            a saúde do solo, como preparo convencional, que revira e nivela o solo para criar uma cama
+                            de semente uniforme, e o plantio direto, onde a semente é plantada em palha, conservando
+                            umidade e reduzindo a erosão. Outras práticas incluem a calagem para corrigir a acidez, a
+                            rotação de culturas para aumentar a fertilidade, e a adubação verde.'
+)
