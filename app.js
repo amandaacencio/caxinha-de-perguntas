8454b867177ebde8010cfc781c885bfdcea598c3
amandaacencio <amanda.acencio@escola.pr.gov.br>
2025-09-24 11:05:30 -0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 function criarCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta,resposta);
+    let container = document.getElementById("container")
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML =
+    <div class="cartao_conteudo">
+                    <h3>country life</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p>como funciona a agricultura?</p>
+                    </div>
+                    <div class="cartao_conteudo_resposta">
+                        <p>A plantação de milho e soja envolve o preparo do solo,
+                            a escolha da semente adequada para as condições locais, o plantio com máquinas específicas
+                            (plantadeiras) e o cuidado constante (tratos culturais) para proteger as plantas de pragas e
+                            doenças. Por fim, a colheita é realizada quando os grãos atingem o ponto ideal, garantindo a
+                            produtividade da lavoura.</p>
+                    </div>
+                </div>
 }
