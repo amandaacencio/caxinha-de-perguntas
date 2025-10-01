48ee5271b3698ad9f58c8bfef930d07f888509c4
amandaacencio <amanda.acencio@escola.pr.gov.br>
2025-10-01 10:39:41 -0300

    .

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 function criarCartao(categoria, pergunta, resposta) {
     let container = document.getElementById("container")
     let cartao = document.createElement('article')
-    let respostaEstaVisivel = false
-    function viraCartao(){
-        respostaEstaVisivel =!respostaEstaVisivel
-    }
-    let respostaEstaVisivel = true
-    cartao.className = 'cartao'
+   cartao.className = 'cartao'
+
     cartao.innerHTML = `
     <div class="cartao_conteudo">
-                    <h3>country life</h3>
+                    <h3>${categoria}</h3>
                     <div class="cartao_conteudo_pergunta">
-                        <p>como funciona a agricultura?</p>
+                        <p>${pergunta}</p>
                     </div>
                     <div class="cartao_conteudo_resposta">
-                        <p>A plantação de milho e soja envolve o preparo do solo,
-                            a escolha da semente adequada para as condições locais, o plantio com máquinas específicas
-                            (plantadeiras) e o cuidado constante (tratos culturais) para proteger as plantas de pragas e
-                            doenças. Por fim, a colheita é realizada quando os grãos atingem o ponto ideal, garantindo a
-                            produtividade da lavoura.</p>
+                        <p>${resposta}</p>
                     </div>
                 </div>
-                `
-                 container.appendChild(cartao)
+              `
+
+ container.appendChild(cartao)  
 
 
-    respostaEstaVisivel = !respostaEstaVisivel
+ let respostaEstaVisivel = false
+    function viraCartao() {
+     respostaEstaVisivel =!respostaEstaVisivel
+     cartao.classList.toggle('ative' , respostaEstaVisivel)
+    }
+
+    let respostaEstaVisivel = true
 }
